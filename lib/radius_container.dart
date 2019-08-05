4b5c39ec8e8f8b14df0646900cdcaea7f7a33fb9
huwentao <hu.wentao@outlook.com>
2019-08-05 15:32:25 +0800

    feat: 为radius_stream_expansion_tile 添加了手动控制参数: manualControl, 当manualControl指定为true是, 则tile只能通过expansionStream来控制

---
+++ lib/radius_container.dart
@@ -0,0 +1,25 @@
+// Copyright 2019/7/26, Hu-Wentao. All rights reserved.
+// Email: hu.wentao@outlook.com
+import 'package:flutter/material.dart';
+
+class radius_container extends StatelessWidget {
+  final Widget child;
+
+  radius_container({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        child: child,
+        borderRadius: BorderRadius.circular(15.0),
+        shadowColor: Colors.grey,
+        elevation: 4,
+      ),
+    );
+  }
+}
